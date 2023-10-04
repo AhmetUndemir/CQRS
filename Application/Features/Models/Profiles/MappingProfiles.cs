dc020cf4df9215dccb7cdcd9247c6c0601eafe68
Ahmet Ündemir <ahmetundemir@outlook.com>
2023-10-04 22:37:39 +0300

    Servise Dynamic query implementasyonları eklendi.

---
+++ Application/Features/Models/Profiles/MappingProfiles.cs
@@ -0,0 +1,35 @@
+﻿using Application.Features.Models.Queries.GetList;
+using Application.Features.Models.Queries.GetListByDynamic;
+using AutoMapper;
+using Core.Application.Responses;
+using Core.Persistence.Paging;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Models.Profiles;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<Model, GetListModelListItemDto>()
+            .ForMember(d => d.BrandName, o => o.MapFrom(s => s.Brand.Name))
+            .ForMember(d => d.FuelName, o => o.MapFrom(s => s.Fuel.Name))
+            .ForMember(d => d.TransmissionName, o => o.MapFrom(s => s.Transmission.Name))
+            .ReverseMap();
+        CreateMap<Paginate<Model>, GetListResponse<GetListModelListItemDto>>().ReverseMap();
+
+
+        CreateMap<Model, GetListByDynamicModelListItemDto>()
+            .ForMember(d => d.BrandName, o => o.MapFrom(s => s.Brand.Name))
+            .ForMember(d => d.FuelName, o => o.MapFrom(s => s.Fuel.Name))
+            .ForMember(d => d.TransmissionName, o => o.MapFrom(s => s.Transmission.Name))
+            .ReverseMap();
+        CreateMap<Paginate<Model>, GetListResponse<GetListByDynamicModelListItemDto>>().ReverseMap();
+
+    }
+}
--- Persistence/PersistenceServiceRegistration.cs
+++ Persistence/PersistenceServiceRegistration.cs
@@ -24,6 +24,10 @@ public static class PersistenceServiceRegistration
         });
 
         services.AddScoped<IBrandRepository, BrandRepository>();
+        services.AddScoped<IModelRepository, ModelRepository>();
+        services.AddScoped<IFuelRepository, FuelRepository>();
+        services.AddScoped<ITransmissionRepository, TransmissionRepository>();
+        services.AddScoped<ICarRepository, CarRepository>();
 
         return services;
     }
